25d0d4f44f56b91ad54d63b2f29f3f8db4ca5285
hitechram <rambala@cisco.com>
2016-11-19 19:46:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,5 @@ app = Flask(__name__)
 def hello():
     return "Hello World from Ram!!"
 
-@app.route("/new")
-def hello():
-    return "Hello World from new Ram!!"
-
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
